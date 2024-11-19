7c34a0d9a80778f42655ace5e564ff160774d0be
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-11-19 09:53:44 +0300

    added initial scripts and NFT contract

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,27 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
-contract RealEstate {
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    //IEnumarable tokenIds
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Real Estate", "REAL") {}
+
+    function mint(string memory _tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        return tokenId;
+    }
+    // how many is the same with the number of Ids assigned
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
 }

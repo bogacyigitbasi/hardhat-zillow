2bbbd6a4314df5d9d05bd08eacda4c3e283a3c46
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-11-19 10:55:53 +0300

    added escrow tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,14 +1,33 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
+
+// there is a real estate token we can rent, and we lock it in an escrow until some set of actions happen
+// the buyer/seller are the ultimate users however the scenario
+// there are lender, inspector and appraiser roles to sign off before the porcess.
+// seller lists the property, buyer deposits earnset, sign off from the appraisal, lender
+// lender funds and transfer the ownershio to buyer
+// seller gets paid
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public lender;
+    address public inspector;
+    address public realEstateTokenAddress;
+    // receives the eth so payable
+    address payable public seller;
 
+    constructor(
+        address _realEstateTokenAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        realEstateTokenAddress = _realEstateTokenAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -4,7 +4,53 @@ const { ethers } = require('hardhat');
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
+let buyer, seller, inspector, lender
+let realEstate, escrow
+beforeEach(async () => {
 
-describe('Escrow', () => {
+    const accounts = await ethers.getSigners()
+    const buyer = accounts[0]
+    seller = accounts[1]
+    inspector = accounts[2]
+    lender = accounts[3]
 
+    // console.log(accounts)
+    // Deploy
+    const RealEstate = await ethers.getContractFactory('RealEstate')
+    realEstate = await RealEstate.deploy()
+
+    console.log(realEstate.address)
+
+    // Mint the real estate on the seller's behalf
+    let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+    await transaction.wait()
+
+
+    // deploy escrow
+    const Escrow = await ethers.getContractFactory('Escrow')
+    escrow = await Escrow.deploy(
+        realEstate.address, seller.address, inspector.address,lender.address
+    )
+})
+    describe('Deployment', () => {
+        it ('Returns NFT address', async()=>{
+            const result = await escrow.realEstateTokenAddress()
+            console.log(result)
+            expect(result).to.be.equal(realEstate.address)
+
+        })
+
+        it ('Returns seller address', async()=>{
+            const sell = await escrow.seller()
+            console.log(sell)
+            console.log(seller.address)
+            expect(sell).to.be.equal(seller.address)
+        })
+
+        it ('Returns inspector', async() => {
+            const ins = await escrow.inspector()
+            console.log(ins)
+            console.log(inspector.address)
+            expect(ins).to.be.equal(inspector.address)
+        })
 })
